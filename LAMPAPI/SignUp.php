18c11d3dc80898df891f8244046b8360dee16265
Ben Braun <benjamindbraun@gmail.com>
2021-01-19 22:56:48 -0500

    finished signup.php

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,48 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "root", "632021Contastic", "Contastic");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+
+	else
+	{
+		$sql = "INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt->execute();
+		if ($result = stmt->execute() != TRUE)
+		{
+			returnWithError($conn->error);
+		}
+		$conn.close();
+	}
+
+	returnWithError("");
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
