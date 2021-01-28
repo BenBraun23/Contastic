3b0b2998e3f16856186d376acec4b8fdff745eff
brandontherrien <43524479+brandontherrien@users.noreply.github.com>
2021-01-27 19:10:06 -0500

    Commited to Website

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "TheBeast", "test", "contastic");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
