770678f73c942a1fb8df6a46055be307df766fc9
Ben Braun <benjamindbraun@gmail.com>
2021-01-29 00:31:19 -0500

    created AddContact endpoint

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,50 @@
+<?php
+	$inData = getRequestInfo();
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$id = $inData["id"];
+
+	$conn = new mysqli("localhost", "root", "632021Contastic", "Contastic");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+
+	else
+	{
+		$sql = "INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID)
+						VALUES (?, ?, ?, ?, ?)";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ssdsd", $firstName, $lastName, $phone, $email, $id);
+		$stmt->execute();
+		$conn->close();
+		returnWithInfo("Contact added");
+	}
+
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo( $message )
+	{
+		$retValue = '{"id":1, "message":"' . $message . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
